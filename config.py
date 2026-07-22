1758741ed2a10c92c938c32035941e9131b60357
vasanth k <vasanthkuncham@gmail.com>
2026-07-22 18:45:58 +0530

    final

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from pathlib import Path
 BASE_DIR=Path(__file__).resolve().parent
 VECTOR_DB_PATH=BASE_DIR / "data" / "vectors"
 METADATA_DB_PATH=BASE_DIR / "data" / "metadata"
-OWASP_DATA_PATH=BASE_DIR / "data" / "payloads.json"
+FILE_DATA_PATH=BASE_DIR / "data" / "payloads.json"
 report_path="/hellhound-spider/hellhound_report.json"
